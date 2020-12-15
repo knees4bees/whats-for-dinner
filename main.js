f05a60b90a6036fc647e83b6dbc2a87745ce5376
Katie Ballentine <knees4bees@gmail.com>
2020-12-15 16:52:29 -0700

    Build some initial html, css, and js before taking a break

---
+++ main.js
@@ -0,0 +1,9 @@
+var body = document.querySelector('body');
+var navBar = document.querySelector('nav');
+var userSelect = document.querySelector('#userSelections');
+var results = document.querySelector('#results');
+
+body.classList.add('backdrop-image');
+navBar.classList.add('teal', 'nav-bar');
+userSelect.classList.add('content-pane');
+results.classList.add('content-pane');

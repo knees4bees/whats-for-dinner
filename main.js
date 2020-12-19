8342a0c944f5a701f268a1f69759b2693b44173f
Katie Ballentine <knees4bees@gmail.com>
2020-12-19 13:29:08 -0700

    Add functionality to display random food of user-chosen type

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var letsCookButton = document.querySelector('.lets-cook-button');
+var cookpot = document.querySelector('.cookpot');
+var youShouldMakeFood = document.querySelector('.you-should-make-food');
+var youShouldMake = document.querySelector('.you-should-make');
+var displayedFood = document.querySelector('.display-food');
+
 
 // Event Listeners
 letsCookButton.addEventListener('click', showMeTheFoody);
@@ -9,8 +14,28 @@ function getRandomIndex(array) {
   return Math.floor(Math.random()*array.length);
 }
 
+function grabArray(componentName) {
+  if (componentName === 'side') {
+    return sides;
+  } else if (componentName === 'main-dish') {
+    return mainDishes;
+  } else {
+    return desserts;
+  }
+}
+
+function hide(item) {
+  item.classList.add('hidden');
+}
+
+function unhide(item) {
+  item.classList.remove('hidden');
+}
 
 function showMeTheFoody() {
-  var userSelection = document.querySelector('input[name="meal-component"]:checked').value;
-  alert(getRandomIndex(userSelection));
+  var type = document.querySelector('input[name="meal-component"]:checked').value;
+  var foods = grabArray(type);
+  displayedFood.innerText = `${foods[getRandomIndex(foods)]}!`;
+  hide(cookpot);
+  unhide(youShouldMakeFood);
 }
